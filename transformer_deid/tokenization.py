56dc7f34ec7f3cc554f9478ca178549e6dc52cec
Walter Nelson <nelsonwa@hhsc.ca>
2024-07-24 06:15:15 -0400

    Unknown error causes hangs rarely; flag this with a quick timeout

--- transformer_deid/tokenization.py
+++ transformer_deid/tokenization.py
@@ -1,11 +1,29 @@
 from bisect import bisect_left, bisect_right
 import logging
+import signal
+from contextlib import contextmanager
 import numpy as np
 import copy
 from typing import List, Optional, Union, TextIO
 
 from tqdm import tqdm
 from transformer_deid.label import Label
+
+
+class TimeoutException(Exception): pass
+
+@contextmanager
+def time_limit(seconds):
+    def signal_handler(signum, frame):
+        raise TimeoutException("Timed out!")
+
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.alarm(seconds)
+    try:
+        yield
+    finally:
+        signal.alarm(0)
+
 
 class DuplicateFilter(logging.Filter):
     def filter(self, record):
@@ -120,33 +138,36 @@ def split_sequences(tokenizer, texts, labels=None, ids=None):
     logger.info('Determining offsets for splitting long segments.')
     for i, encoded in tqdm(enumerate(encodings.encodings),
                            total=len(encodings.encodings)):
-        offsets = [o[0] for o in encoded.offsets]
-        token_sw = [False] + [
-            encoded.word_ids[i + 1] == encoded.word_ids[i]
-            for i in range(len(encoded.word_ids) - 1)
-        ]
-        # iterate through text and add create new subsets of the text
-        start = 0
-        subseq = []
-        while start < len(offsets):
-            # ensure we do not start on a sub-word token
-            while token_sw[start]:
-                start -= 1
+        with time_limit(10):
+            offsets = [o[0] for o in encoded.offsets]
+            token_sw = [False] + [
+                encoded.word_ids[i + 1] == encoded.word_ids[i]
+                for i in range(len(encoded.word_ids) - 1)
+            ]
+            # iterate through text and add create new subsets of the text
+            start = 0
+            subseq = []
+            while start < len(offsets):
+                # ensure we do not start on a sub-word token
+                while token_sw[start]:
+                    start -= 1
 
-            stop = start + seq_len
-            if stop < len(offsets):
-                # ensure we don't split sequences on a sub-word token
-                # do this by shortening the current sequence
-                while token_sw[stop]:
-                    stop -= 1
-            else:
-                # end the sub sequence at the end of the text
-                stop = len(offsets)
+                stop = start + seq_len
+                if stop < len(offsets):
+                    # ensure we don't split sequences on a sub-word token
+                    # do this by shortening the current sequence
+                    while token_sw[stop]:
+                        stop -= 1
+                else:
+                    # end the sub sequence at the end of the text
+                    stop = len(offsets)
 
-            subseq.append(start)
+                subseq.append(start)
 
-            # update start of next sequence to be end of current one
-            start = stop
+                # update start of next sequence to be end of current one
+                start = stop
+        except TimeoutException:
+            subseq = [0]
 
         sequence_offsets.append(subseq)
 
